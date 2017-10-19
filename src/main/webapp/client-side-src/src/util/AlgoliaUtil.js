cd766b4f523a7293c49a659c987ffb0d3fd582a1
Nodirbek <nodirbek.shamsiyev@maill.ru>
2017-10-19 16:20:53 +0900

    hide keys fix commit

--- src/main/webapp/client-side-src/src/util/AlgoliaUtil.js
+++ src/main/webapp/client-side-src/src/util/AlgoliaUtil.js
@@ -2,6 +2,6 @@ import {createFromAlgoliaCredentials} from 'vue-instantsearch';
 import algoliasearch from 'algoliasearch';
 
 
-export const searchStore = createFromAlgoliaCredentials('TY70EG43AB', 'ad50754214a919e6c5a9b352dbaac6d8');
+export const searchStore = createFromAlgoliaCredentials('', '');
 
-export const adminIndex = algoliasearch('TY70EG43AB', '0ad56bad0eb24f79800a0a1c5953ae8d').initIndex('books');
+export const adminIndex = algoliasearch('', '').initIndex('books');
