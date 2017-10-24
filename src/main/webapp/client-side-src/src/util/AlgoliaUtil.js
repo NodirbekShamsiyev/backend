e86ea19a78964004d45cdea57445f2e98d498067
Nodirbek <nodirbek.shamsiyev@maill.ru>
2017-10-24 16:10:55 +0900

    'ignore'

--- src/main/webapp/client-side-src/src/util/AlgoliaUtil.js
+++
@@ -1,7 +0,0 @@
-import {createFromAlgoliaCredentials} from 'vue-instantsearch';
-import algoliasearch from 'algoliasearch';
-
-
-export const searchStore = createFromAlgoliaCredentials('', '');
-
-export const adminIndex = algoliasearch('', '').initIndex('books');
