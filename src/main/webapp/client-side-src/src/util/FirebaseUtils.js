5f59601b3358e242524b42caf8c3100515a304b2
Nodirbek <nodirbek.shamsiyev@maill.ru>
2017-10-24 16:09:45 +0900

    file removed

--- src/main/webapp/client-side-src/src/util/FirebaseUtils.js
+++ src/main/webapp/client-side-src/src/util/FirebaseUtils.js
@@ -1,10 +1,3 @@
 import * as firebase from "firebase";
 
-export const Firebase = firebase.initializeApp({
-    apiKey: "AIzaSyD1Gw56VX47nQbUdTAEjidFivEKkRVhNgI",
-    authDomain: "it-convergence-d39b2.firebaseapp.com",
-    databaseURL: "https://it-convergence-d39b2.firebaseio.com",
-    projectId: "it-convergence-d39b2",
-    storageBucket: "it-convergence-d39b2.appspot.com",
-    messagingSenderId: "503178303321"
-});
+export const Firebase = firebase.initializeApp({});
